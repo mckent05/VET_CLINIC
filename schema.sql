6fa7d726e38840948623236a7c93229f3a2614de
MCKENT05 <atemitope95@gmail.com>
2022-01-10 12:41:18 +0100

    created table for vet_clinic db

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
+postgres=# CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
-    name varchar(100)
+    vet_clinic(# id INT GENERATED ALWAYS AS IDENTITY,
+    vet_clinic(# name VARCHAR(100),
+    vet_clinic(# date_of_birth date,
+    vet_clinic(# escape_attempts int,
+    vet_clinic(# neutered boolean,
+    vet_clinic(# weight_kg decimal,
+    vet_clinic(# PRIMARY KEY(id)
 );
