7068a4ab9f979a75100328d8f9272df6fa929c7f
MCKENT05 <atemitope95@gmail.com>
2022-01-10 14:56:33 +0100

    queried data from vet_clinic db

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-# curriculum-databases-projects-template
+# VET_CLINIC
 
-> This template should be used for database related projects at Microverse.
-> Generate your own repository, update this README and edit all files content while working on projects. You should not be adding any new files unless asked otherwise.
+> A simple database for animals.
 
 
 ## Getting Started
@@ -15,17 +14,9 @@ This repository includes files with plain SQL that can be used to recreate a dat
 
 ## Authors
 
-👤 **Author1**
-
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
-
-👤 **Author2**
-
-- GitHub: [@githubhandle](https://github.com/githubhandle)
-- Twitter: [@twitterhandle](https://twitter.com/twitterhandle)
-- LinkedIn: [LinkedIn](https://linkedin.com/in/linkedinhandle)
+- GitHub: [@githubhandle](https://github.com/mckent05)
+- Twitter: [@twitterhandle](https://twitter.com/mckent05)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/akinladetemitope)
 
 ## 🤝 Contributing
 
--- queries.sql
+++ queries.sql
@@ -1,3 +1,27 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+--Find all animals whose name ends in "mon".
+vet_clinic=# SELECT * FROM animals where name LIKE '%mon%';
+
+--List the name of all animals born between 2016 and 2019
+vet_clinic=# SELECT name FROM animals where date_of_birth BETWEEN '2016-1-1' AND '2019-12-1';
+
+--List the name of all animals that are neutered and have less than 3 escape attempts.
+vet_clinic=# SELECT name FROM animals where neutered = true and escape_attempts < 3;
+
+--List date of birth of all animals named either "Agumon" or "Pikachu".
+vet_clinic=# SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
+
+--List name and escape attempts of animals that weigh more than 10.5kg
+vet_clinic=# SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+
+--Find all animals that are neutered.
+vet_clinic=# SELECT * FROM animals WHERE neutered = true;
+
+--Find all animals not named Gabumon.
+vet_clinic=# SELECT * FROM animals WHERE name != 'Gabumon';
+
+--Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
+vet_clinic=# SELECT * FROM animals WHERE weight_kg >=10.4 AND weight_kg <= 17.3;
+
+
