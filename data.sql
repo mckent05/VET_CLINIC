1bc016f6754cbd44fdee0068e402cd936f6abf25
MCKENT05 <atemitope95@gmail.com>
2022-01-11 13:22:18 +0100

    finished all task for updating table

--- data.sql
+++ data.sql
@@ -6,4 +6,17 @@ INSERT INTO animals (name, date_of_birth, neutered, weight_kg, escape_attempts) 
 
 INSERT INTO animals (name, date_of_birth, neutered, weight_kg, escape_attempts) VALUES('Pikachu', '2021-1-7', false, 15.04, 1);
 
-INSERT INTO animals (name, date_of_birth, neutered, weight_kg, escape_attempts) VALUES('Devimon', '2017-5-12', true, 11, 5);
+INSERT INTO animals (name, date_of_birth, neutered, weight_kg, escape_attempts) VALUES('Devimon', '2017-5-12', true, 11, 5
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Charmander', '2020-2-8', 0, false, 11);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Plantmon', '2022-9-15', 2, true, 5.7);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Squirtile', '1993-4-2', 3, false, 12.13);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Angemon', '2005-6-12', 1, true, 45);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Boarmon','2005-6-7', 7, true, 20.4);
+
+vet_clinic=# INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Blossom','1998-10-13', 3, true, 17);
+);
--- queries.sql
+++ queries.sql
@@ -24,4 +24,48 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 --Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE weight_kg >=10.4 AND weight_kg <= 17.3;
 
+--How many animals are there
+SELECT COUNT(*) FROM animals;
+--  count
+-------
+--     9
+--(1 row)
+
+--How many animals have never tried to escape
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+--  count
+-------
+--  2
+--(1 row)
+
+--what is the avergae weight of all animals
+SELECT AVG(weight_kg) FROM animals;
+-- avg
+---------------------
+-- 16.6444444444444444
+-- (1 row)
+
+--who escapes most beween neutered and not neutered animals
+SELECT SUM(escape_attempts), neutered FROM animals GROUP BY (neutered);
+ --sum | neutered
+-----+----------
+ --  4 | f
+ -- 18 | t
+--(2 rows)
+
+--What is the mininum and maximum weight of each type of animal
+SELECT MAX(weight_kg), MIN(weight_kg), species FROM animals GROUP BY (species);
+-- max | min | species
+-----+-----+---------
+ -- 17 |  11 | pokemon
+ -- 45 |   8 | digimon
+--(2 rows)
+
+--What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-1-1' AND '2000-12-31' GROUP BY(species);
+--species |        avg
+---------+--------------------
+ --pokemon | 3.0000000000000000
+--(1 row)
+
 
