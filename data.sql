37879f32268c040e15d8ba3d9afba44eff87e5db
MCKENT05 <atemitope95@gmail.com>
2022-01-12 14:43:12 +0100

    added owner_id foreign key to animals table and corrected owner_id in data.sql

--- data.sql
+++ data.sql
@@ -51,4 +51,4 @@ UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
 
  UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtile', 'Blossom');
 
- UPDATE animals SET owner_id = 4 WHERE name IN ('Angemon', 'Boarmon');
+ UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
