5dcd7b7e9c1280caedffbd7cedb1c10bd9fcb1d9
MCKENT05 <atemitope95@gmail.com>
2022-01-13 10:44:03 +0100

    added join-table for visits and completed all queries

--- data.sql
+++ data.sql
@@ -52,3 +52,61 @@ UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
  UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtile', 'Blossom');
 
  UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+
+
+ INSERT INTO vets (name, age, date_of_graduation) VALUES('William Tacher', 45, '2000-5-23');
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES('Maisy Smith', 26, '2019-1-17');
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES('Stephanie Mendez', 64, '1981-5-4');
+
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Jack Harkness', 38, '2008-6-8');
+
+INSERT INTO specialization VALUES (1, 3);
+
+INSERT INTO specialization VALUES (3, 3);
+
+INSERT INTO specialization VALUES (3, 4);
+
+INSERT INTO specialization VALUES (4, 4);
+
+INSERT INTO visits VALUES (1, 1, '2020-5-24');
+
+INSERT INTO visits VALUES(3,1, '2020-7-22'); 
+
+INSERT INTO visits VALUES (4, 2, '2021-2-2');
+
+INSERT INTO visits VALUES (2, 3, '2020-1-5');
+
+INSERT INTO visits VALUES (2, 3, '2020-3-8');
+
+INSERT INTO visits VALUES (2, 3, '2020-5-14');
+
+INSERT INTO visits VALUES (4, 3, '2021-5-4');
+
+INSERT INTO visits VALUES ( (SELECT id FROM vets WHERE name = 'Jack Harness'),( SELECT id FROM animals WHERE name = 'Charmander'), '2021-2-24');
+
+INSERT INTO visits VALUES ( (SELECT id FROM vets WHERE name = 'Maisy Smith'),( SELECT id FROM animals WHERE name = 'Plantmon'), '2019-12-24');
+
+INSERT INTO visits VALUES ( (SELECT id FROM vets WHERE name = 'William Tatcher'),( SELECT id FROM animals WHERE name = 'Plantmon'), '2020-8-10');
+
+INSERT INTO visits VALUES ( (SELECT id FROM vets WHERE name = 'Maisy Smith'),( SELECT id FROM animals WHERE name = 'Plantmon'), '2021-4-7');
+
+INSERT INTO visits VALUES ( (SELECT id FROM vets WHERE name = 'Stephanie Mendez'),( SELECT id FROM animals WHERE name = 'Squirtle'), '2019-9-29');
+
+INSERT INTO visits VALUES ( (SELECT id FROM vets WHERE name = 'Jack Harkness'),( SELECT id FROM animals WHERE name = 'Angemon'), '2020-10-3');
+
+INSERT INTO visits VALUES ( (SELECT id FROM vets WHERE name = 'Jack Harkness'),( SELECT id FROM animals WHERE name = 'Angemon'), '2020-11-4');
+
+INSERT INTO visits VALUES ( (SELECT id FROM vets WHERE name = 'Maisy Smith'),( SELECT id FROM animals WHERE name = 'Boarmon'), '2019-1-24');
+
+INSERT INTO visits VALUES ( (SELECT id FROM vets WHERE name = 'Maisy Smith'),( SELECT id FROM animals WHERE name = 'Boarmon'), '2019-5-15');
+
+INSERT INTO visits VALUES ( (SELECT id FROM vets WHERE name = 'Maisy Smith'),( SELECT id FROM animals WHERE name = 'Boarmon'), '2020-2-27');
+
+INSERT INTO visits VALUES ( (SELECT id FROM vets WHERE name = 'Maisy Smith'),( SELECT id FROM animals WHERE name = 'Boarmon'), '2020-8-3');
+
+ INSERT INTO visits VALUES ( (SELECT id FROM vets WHERE name = 'Stephanie Mendez'),( SELECT id FROM animals WHERE name = 'Blossom'), '2020-5-24');
+
+ INSERT INTO visits VALUES ( (SELECT id FROM vets WHERE name = 'William Tatcher'),( SELECT id FROM animals WHERE name = 
+'Blossom'), '2021-1-11');
--- schema.sql
+++ schema.sql
@@ -35,3 +35,23 @@ ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
 ALTER TABLE animals ADD COLUMN owner_id INT;
 
 ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
+
+
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50),
+    age INT,
+    date_of_graduation date,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE specialization (      
+    vet_id INT REFERENCES vets (id),   
+    species_id INT REFERENCES species (id)
+);
+
+CREATE TABLE visits (
+    vet_id INT REFERENCES vets (id),
+    animal_id INT REFERENCES animals(id),
+    visit_date date
+);
