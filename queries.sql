fbea49aa5174e9b3d90252161de5746ddbf4cb72
hamid-murambiwa <machipisajunior@gmail.com>
2022-01-17 17:34:33 +0200

    Add indexes to the first and second query and add screen shots of the results

--- queries.sql
+++ queries.sql
@@ -108,6 +108,9 @@ SELECT animals.species_id, species.name FROM visits JOIN vets ON visits.vet_id =
 
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits WHERE animal_id = 4;
 
+EXPLAIN ANALYZE SELECT * FROM visits WHERE vet_id = 2;
+
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
 
 
 
